884b946595f8bcbda05d2d0124498c0ddfb23d43
vuhk <vu.ho@onemount.com>
2021-06-15 17:25:01 +0700

    format after gen

--- cmd/genapi/gen_api.go
+++ cmd/genapi/gen_api.go
@@ -82,14 +82,14 @@ func (g *Generator) exec() error {
 	}
 
 	g.connect()
-	m, err := g.inspect()
+	model, err := g.inspect()
 	if err != nil {
 		return err
 	}
-	if err := g.generateTemplate(modelTemplate, modelDir, m); err != nil {
+	if err := g.generateTemplate(modelTemplate, modelDir, model); err != nil {
 		return err
 	}
-	repo := m.ToDbRepo()
+	repo := model.ToDbRepo()
 
 	if err := g.generateTemplate(repoTemplate, repoDir, repo); err != nil {
 		return err
@@ -102,6 +102,7 @@ func (g *Generator) exec() error {
 	if err := g.generateTemplate(controllerTemplate, controllerDir, controller); err != nil {
 		return err
 	}
+	g.format()
 
 	return nil
 }
@@ -125,11 +126,13 @@ func (g *Generator) close() {
 func (g *Generator) inspect() (*models.Model, error) {
 	q, err := g.db.Prepare("SELECT `COLUMN_NAME`, `DATA_TYPE`, `IS_NULLABLE`, `COLUMN_KEY` FROM `COLUMNS` WHERE `TABLE_SCHEMA` = ? AND `TABLE_NAME` =?")
 	if err != nil {
+		zap.S().Errorw("Error when create prepare statement", "error", err)
 		return nil, err
 	}
 
 	rows, err := q.Query(g.DBName, g.DBTable)
 	if err != nil {
+		zap.S().Errorw("Error when exec query", "error", err)
 		return nil, err
 	}
 
@@ -172,6 +175,7 @@ func (g *Generator) inspect() (*models.Model, error) {
 
 func (g *Generator) createFile(file string) (*os.File, error) {
 	if err := os.MkdirAll(filepath.Dir(file), 0770); err != nil {
+		zap.S().Errorw("Error when create file", "error", err, "file", file)
 		return nil, err
 	}
 	return os.Create(file)
@@ -180,12 +184,14 @@ func (g *Generator) createFile(file string) (*os.File, error) {
 func (g *Generator) getModulePath() error {
 	cmd := exec.Command("bash", "-c", "go mod edit -json > gomod.json")
 	if err := cmd.Run(); err != nil {
+		zap.S().Errorw("Error when exec command", "error", err)
 		return err
 	}
 	defer exec.Command("bash", "-c", "rm -f gomod.json").Run()
 
 	f, err := os.Open("gomod.json")
 	if err != nil {
+		zap.S().Errorw("Error when open gomod.json", "error", err)
 		return err
 	}
 	defer f.Close()
@@ -197,6 +203,7 @@ func (g *Generator) getModulePath() error {
 	}
 
 	if err := json.NewDecoder(f).Decode(&mod); err != nil {
+		zap.S().Errorw("Error when decode gomod.json", "error", err)
 		return err
 	}
 
@@ -210,10 +217,12 @@ func (g *Generator) getModulePath() error {
 func (g *Generator) generateTemplate(layout, folder string, data interface{}) error {
 	tmpl, err := template.New("tmpl").Parse(layout)
 	if err != nil {
+		zap.S().Errorw("Error when parse layout", "folder", folder, "error", err)
 		return err
 	}
 	fo, err := g.createFile(fmt.Sprintf("%v/%v/%v.go", g.OutputFolder, folder, g.DBTable))
 	if err != nil {
+		zap.S().Errorw("Error when create file", "output", g.OutputFolder, "folder", folder, "table", g.DBTable, "error", err)
 		return err
 	}
 
@@ -225,9 +234,18 @@ func (g *Generator) generateTemplate(layout, folder string, data interface{}) er
 
 	err = tmpl.Execute(fo, data)
 	if err != nil {
+		zap.S().Errorw("Error when exec template", "error", err)
 		return err
 	}
 	return nil
+}
+
+func (g *Generator) format() {
+	cmd := exec.Command("gofmt","-w", g.OutputFolder)
+	zap.S().Infow("cmd", "cmd",cmd.String())
+	if err := cmd.Run(); err != nil {
+		zap.S().Warnw("Error when format output", "error", err)
+	}
 }
 
 func GetGoDataType(mysqlType, isNullable string) string {
